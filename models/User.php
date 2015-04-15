8c99d66875dc5017f33b2cf32fedf25f3675b293
Alex Maslov <unseensa@gmail.com>
2015-04-15 16:16:24 +0300

    access user model

--- models/User.php
+++ models/User.php
@@ -18,6 +18,13 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
             'authKey' => 'test100key',
             'accessToken' => '100-token',
         ],
+//        '101' => [
+//            'id' => '101',
+//            'username' => 'newUserLogin',
+//            'password' => 'newuserPassword',
+//            'authKey' => 'test101key',
+//            'accessToken' => '101-token',
+//        ],
     ];
 
     /**
