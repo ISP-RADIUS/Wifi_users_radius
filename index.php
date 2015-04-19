f40d29856712076b008aae3e1f2e44f6b74f0ce0
Ihor Dutchak <ihor.youw@gmail.com>
2015-04-20 00:49:55 +0200

    add redirection and 404

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>redirecting...</title>
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+        <meta http-equiv="refresh" content="0; url=/main/list/" />
+</head>
+<body>
+</body>
+</html>
